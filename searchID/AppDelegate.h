5bdbd9b64594c9dea6e1173a3f35914be4b8215f
Again <again@mac-mini.local>
2016-04-12 22:29:34 +0800

    Initial Commit

---
+++ searchID/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  searchID
+//
+//  Created by again on 16/4/12.
+//  Copyright © 2016年 again. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ searchID/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  searchID
+//
+//  Created by again on 16/4/12.
+//  Copyright © 2016年 again. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
